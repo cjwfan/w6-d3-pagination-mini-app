49be729bdf47cf82357b01ac8cd47c04da4b5d38
Mia C <mcollette101@gmail.com>
2026-03-13 14:41:44 -0500

    added Get function to get data

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+async function getCharacterPage(url) {
+    try {
+        const res = await fetch(url);
+        
+          if (res.status !== 200) {
+            throw new Error("Failed to fetch");
+          }
+        const data = await res.json();
+        // console.log(data);
+        return data;    //remove later??
+    } catch (error) {
+        console.error(error)
+    }
+      
+}
+
+
