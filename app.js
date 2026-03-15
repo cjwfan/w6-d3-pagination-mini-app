e4216317929173f322af774c036d58e9642e1905
Mia C <mcollette101@gmail.com>
2026-03-14 19:51:18 -0500

    added page 1 of results

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//step 1
 async function getCharacterPage(url) {
     try {
         const res = await fetch(url);
@@ -16,13 +17,49 @@ async function getCharacterPage(url) {
       
 }
 
+//step 3
+function renderCharacters (characters){
+    const outputDisplay = document.getElementById("output")
+    outputDisplay.textContent = "";
+    //step 4 (image and name)
+    for (const character of characters.slice(0,10)) {
 
+      const p = document.createElement("p")
+      p.textContent = character.name 
+      const img = document.createElement("img");
+      img.src = character.image 
+      img.alt = character.name
+
+      outputDisplay.appendChild(p)
+
+      outputDisplay.appendChild(p);
+      outputDisplay.appendChild(img);
+      }
+
+
+    }
+
+
+   
+
+
+
+//Step 2
 async function main() {
+
+    const loadingMessage =document.getElementById("loadingMessage");
+    // const ouput = document.getElementById("output");
   try {
-    const characters = getCharacterPage(
-      "https://rickandmortyapi.com/api/character",
+    loadingMessage.textContent = "Loading..."
+
+    const characters = await getCharacterPage(
+      "https://rickandmortyapi.com/api/character"
+      
     );
     console.log(characters);
+    renderCharacters(characters.results)
+   
+    
   } catch (error) {}
 }
 
